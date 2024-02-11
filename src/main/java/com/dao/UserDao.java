2ac1afae568d873d52222002d405e33a22f1cffe
RautuA <rautualexandruflorin@gmail.com>
2024-02-11 11:48:58 +0200

    Added login functionality in UserDao to retrieve user information based on email and password parameters

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,7 +1,11 @@
 package com.dao;
 import org.hibernate.SessionFactory;
+
+
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.Query;
 
 import com.entity.User;
 public class UserDao {
@@ -40,4 +44,27 @@ public class UserDao {
 	}
 	return f;
 }
+
+
+
+public User login(String email, String password) {
+
+  User u = null;
+  session = factory.openSession();
+  
+  Query q = session.createQuery("from User where email:em and password:ps");
+  
+  q.setParameter("em", email);
+  q.setParameter("ps", password);
+  
+  u = (User) q.uniqueResult();
+  
+  
+  
+  
+  
+  return u;
 }
+}
+
+
